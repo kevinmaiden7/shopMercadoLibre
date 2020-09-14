11434605813a05fef3a25544a7a471a4be6550ad
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-09-14 12:33:17 -0500

    Setting router v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'product-details',
+    component: ProductDetailsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
